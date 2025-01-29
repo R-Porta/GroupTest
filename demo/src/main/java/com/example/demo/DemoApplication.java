50e6e890f031270808060e03940a3ae945a7e3a8
gadie <gadie@10.0.0.28>
2025-01-29 09:19:33 +0100

    que pasa wey

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,14 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+	public static double Number(int a, int b) {
+		int a = 10;
+		int b = 20;
+	}
+		
 	}
 
 }
+
+
